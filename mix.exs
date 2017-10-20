0a5096dd9b1b4376dd489d7c3e9a8ef346f8f0f4
Serge Smetana <smetana@pluron.com>
2017-10-20 15:34:00 +0300

    Extensive test example

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule Example.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:floki, "~> 0.7", only: :test}
     ]
   end
 end
--- test/example_web/controllers/page_controller_test.exs
+++ test/example_web/controllers/page_controller_test.exs
@@ -3,6 +3,16 @@ defmodule ExampleWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200)
+    body = html_response(conn, 200)
+    title = Floki.find(body, "title")
+      |> Floki.text
+    assert title == "Hello Example!"
+    header = Floki.find(body, "h2")
+      |> Floki.text
+    assert header == "Welcome to Phoenix!"
+    sections = Floki.find(body, "h4")
+      |> Enum.map(&Floki.text/1)
+    assert sections == ["Resources", "Help"]
   end
 end
